df60b68242b409d7ad67954c4ea44a0f3d7b6e67
Administrator <1019950982@qq.com>
2025-06-24 16:36:55 +0800

    2025.6.24。初始化多模块

---
+++ admin/src/main/java/com/shortLink/dao/mapper/GroupUniqueMapper.java
@@ -0,0 +1,10 @@
+package com.shortLink.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shortLink.dao.entity.GroupUniqueDO;
+
+/**
+ * 短链接分组唯一路由持久层
+ */
+public interface GroupUniqueMapper extends BaseMapper<GroupUniqueDO> {
+}
---
+++ admin/src/main/java/com/shortLink/dao/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.shortLink.dao.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shortLink.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}
